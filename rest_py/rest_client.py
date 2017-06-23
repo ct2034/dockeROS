c805bf2ccbe846e71316f4d10a30c08906714525
Poulastya Mukherjee <poulastya.mukherjee@ipa.fraunhofer.de>
2017-06-23 17:33:01 +0200

    get back ip address and store in a mongodb database

--- rest_py/rest_client.py
+++ rest_py/rest_client.py
@@ -10,8 +10,9 @@ import datetime
 import subprocess
 import os
 import ast
+from uuid import getnode as get_mac
 
-get_ip = 'http localhost:8000/things >> tmpfile'
+get_ip = 'http 10.2.1.11:8000/things >> tmpfile'
 subprocess.call(get_ip, shell=True)
 pathtmpfile = os.path.abspath("tmpfile")
 
@@ -22,6 +23,8 @@ tmpfile.close()
 os.remove(pathtmpfile)
 dict_pkp = ast.literal_eval(pkg_path)
 print dict_pkp['ip_add']
+mac = get_mac()
+dict_pkp['mac_add']=mac
 #dict_pkp_lvl2 = (dict_pkp['ip_add'])#ast.literal_eval(dict_pkp['ip_add'])
 #print dict_pkp['ip_add']
 #dict_pkp_lvl2 = ast.literal_eval(tmp)
@@ -29,7 +32,7 @@ print dict_pkp['ip_add']
 #print pkg_path['ip_add']# = pkg_path[:-1]
 
 
-"""
+
 
 client = pymo.MongoClient('localhost', 27017)
 db = client.test_database
@@ -39,12 +42,11 @@ post = {"author": "Mike",
         "date": datetime.datetime.utcnow()}
 
 posts = db.posts
-post_id = posts.insert(post)
+post_id = posts.insert(dict_pkp)
 post_id
 
 print db.collection_names()
 
 print "################################"
-
-print posts.find_one()
-"""
+for post in posts.find():
+    print post
