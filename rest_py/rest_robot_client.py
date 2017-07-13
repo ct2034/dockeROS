7a2d3c341096e35c218663ab7f9a47b176badff4
Poulastya Mukherjee <poulastya.mukherjee@ipa.fraunhofer.de>
2017-07-13 19:46:11 +0200

    added comments

--- rest_py/rest_robot_client.py
+++ rest_py/rest_robot_client.py
@@ -10,19 +10,21 @@ import socket
 from uuid import getnode as get_mac
 import subprocess
 
+"""Getting IP address """
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 doc = {'ip_add':s.getsockname()[0]}
+
+"""Getting mac address for uuid"""
 mac = get_mac()
 doc['uuid']=mac
 
+"""Writing a JSON file"""
 with open('/home/robotino/rosedge/rest_py/rob_config.json','w') as outfile:
 	json.dump(doc,outfile)
 
-
-
+"""Sending Json command to Server Robot"""
 put_ip = 'http PUT 10.2.1.10:8000/things < rob_config.json'
-
 subprocess.call(put_ip, shell=True)
 
 
--- rest_py/rest_server_pc.py
+++ rest_py/rest_server_pc.py
@@ -13,31 +13,34 @@ import pymongo as pymo
 import datetime
 from falcon_multipart.middleware import MultipartMiddleware
 
-class ThingsResource(object):
+"""
+Server class currently handles GET and PUT requests using Falcon REST API
+"""
+class Robconnect(object):
+    
+    """Handles GET requests"""
     def on_get(self, req, resp):
-        """Handles GET requests"""
+        
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect(("8.8.8.8", 80))
         doc = {'ip_add':s.getsockname()[0]}
         s.close()
         resp.status = falcon.HTTP_200  # This is the default status
         resp.body = json.dumps(doc, ensure_ascii=False)
-        
+     
+    """Handles PUT requests and stores incoming data it in Mongodb database"""
     def on_put(self, req, resp):
         print "on put"
-        print req.get_param('uuid')
         body = req.stream.read()
-        print "on put1"
+        
         if not body:
-            raise falcon.HTTPBadRequest('Empty request body','A valid JSON document is required.')
+            raise falcon.HTTPBadRequest('Empty request body',
+                                        'A valid JSON document is required.')
         
         try:
             req.context['doc'] = json.loads(body.decode('utf-8'))
-            print "on put2"
             request_body = req.stream.read()
-            print "on put3"
             req.json = json.loads(body)
-            print "on put4"
             print request_body
             
         except KeyError:
@@ -46,27 +49,27 @@ class ThingsResource(object):
                 'A thing must be submitted in the request body.')
 
         tmpv = ast.literal_eval(body)
+        tmpv['time'] = datetime.datetime.utcnow()
+        
+        """Connecting to Database"""
         client = pymo.MongoClient('localhost', 27017)
         db = client.test_database
         
-
-        tmpv['time'] = datetime.datetime.utcnow()
         posts = db.posts
         post_id = posts.insert(tmpv)
         post_id
         print db.collection_names()
         for post in posts.find():
             print post
-        #post_id
         resp.status = falcon.HTTP_201
-        
         resp.body = "OK!"
 
-# falcon.API instances are callable WSGI apps
+
+"""initializing falcon app"""
 app = falcon.API(middleware=[MultipartMiddleware()])
-print "sts"
+print "Server started successfully!"
 # Resources are represented by long-lived class instances
-things = ThingsResource()
+things = Robconnect()
 
 # things will handle all requests to the '/things' URL path
 app.add_route('/things', things)
